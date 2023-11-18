7fb0b31bdd5990d60548e7a2ddf98468a42e717c
Yevgen Sorokin <yevgen.sorokin@similarweb.com>
2023-11-18 17:30:35 +0200

    Add welcomeUser route

--- src/Controller/WelcomeController.php
+++ src/Controller/WelcomeController.php
@@ -11,8 +11,14 @@ class WelcomeController extends AbstractController
     #[Route('/welcome')]
     public function index(): Response
     {
-        return $this->render('welcome/index.html.twig', [
-            'name' => 'Yevgen',
+        return $this->render('welcome/index.html.twig');
+    }
+
+    #[Route('/welcome/{name}')]
+    public function welcomeUser(string $name): Response
+    {
+        return $this->render('welcome/user.html.twig', [
+            'name' => $name,
         ]);
     }
 }
